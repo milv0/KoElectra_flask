fd4a8c2ac0ffc7ab8236d684c14214e0ba099ac1
박달루 <127670741+milv0@users.noreply.github.com>
2024-07-18 15:08:38 +0900

    Create emotion.py

---
+++ model/emotion.py
@@ -0,0 +1,12 @@
+class Emotion:
+    def __init__(self):
+        pass
+
+    JOY = "joy"
+    HOPE = "hope"
+    NEUTRALITY = "neutrality"
+    SADNESS = "sadness"
+    ANGER = "anger"
+    ANXIETY = "anxiety"
+    TIREDNESS = "tiredness"
+    REGRET = "regret"
